d5d257f68a065f84e78dbab4b8e3299c10c68480
masanag <masaki.nagaishi@gmail.com>
2016-10-30 18:54:36 +0900

    「ガード」まで

--- sqrt0.hs
+++ sqrt0.hs
@@ -3,7 +3,10 @@ sqrt' x = sqrtBool (x < 0) x
 sqrtBool True _ = Nothing
 sqrtBool _ x = Just (sqrt x)
 
-sqrt' x
+sqrt2' x
   | x < 0 = Nothing
   | otherwise = Just (sqrt x)
 
+div3 x
+  | x `mod` 3 == 0 = x `div` 3
+  | otherwise = x
