e0756f8594e32fa53f3edf526c21ecc6ab408573
masanag <masaki.nagaishi@gmail.com>
2016-11-06 13:27:22 +0900

    Add maybe2 function

--- maybe.hs
+++ maybe.hs
@@ -5,3 +5,7 @@ fromMaybe' d _ = d
 maybe' :: b -> (a -> b) -> Maybe a -> b
 maybe' _ f (Just x) = f x
 maybe' d _ _ = d
+
+maybe2 :: b -> (a -> a -> b) -> Maybe a -> Maybe a -> b
+maybe2 _ f (Just x) (Just y) = f x y
+maybe2 d _ _ _ = d
