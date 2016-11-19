bd5bb69195af451e1cf08de26e68e273b2b88eda
masanag <masaki.nagaishi@gmail.com>
2016-11-19 17:46:44 +0900

    ここまでのまとめ完了

--- revision.hs
+++ revision.hs
@@ -1,3 +1,29 @@
 qux x
   | x <= 0 = 0
   | otherwise = x * 2
+
+fromMaybe' :: a -> Maybe a -> a
+fromMaybe' _ (Just x) = x
+fromMaybe' d Nothing = d
+
+maybe' :: b -> (a -> b) -> Maybe a -> b
+maybe' _ f (Just x) = f x
+maybe' d _ _ = d
+
+id' :: a -> a
+id' x = x
+
+const' :: a -> b -> a
+const' x _ = x
+
+dol' :: (a -> b) -> a -> b
+dol' f = f
+
+dot' :: (b -> c) -> (a -> b) -> a -> c
+dot' f g x = f (g x)
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f x y = f y x
+
+on' :: (b -> b -> c) -> (a -> b) -> a -> a -> c
+on' f g x y = f (g x) (g y)
